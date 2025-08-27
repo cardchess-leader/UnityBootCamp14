d903a5ce35223e05cf28152e2c11f2427009df29
Board Collie <cardchess.leader@gmail.com>
2025-08-27 17:47:22 +0900

    Some packages cleanup

--- DogFight/Assets/Scripts/GameManager.cs
+++ DogFight/Assets/Scripts/GameManager.cs
@@ -60,7 +60,7 @@ public class GameManager : MonoBehaviour
 // 2. Add some kind of scoring system (Done)
 // 3. Add sound effects for shooting and explosions (Download assets from Unity Asset Store)
 // 4. Add background music 
-// 5. Implement enemy attack behavior (e.g., enemy planes shooting at the player)
+// 5. Implement enemy attack behavior (Done)
 // 6. Add Player health and Game Over conditions (Player Health done, game over not done)
 // 7. Implement a simple main menu and pause menu
 // 8. Optimize performance (e.g., object pooling for bullets and enemies. Maybe explosions too?)
@@ -70,6 +70,8 @@ public class GameManager : MonoBehaviour
 // 12. Implement level progression with increasing difficulty
 // 13. Add Rear View Camera (Done)
 // 14. Add reloading message when reloading (Done)
+// 15. Game UI Improvements
+// 16. Find fonts that fits the low-poly art style
 
 // Bugfix
 // 1. When shooting bullets, sudden move when tilting sideways (For player airplane)
--- DogFight/README.md
+++ DogFight/README.md
@@ -1,2 +1,9 @@
 # UnityBootCamp14
 UnityBootCamp14 Projects
+
+Import these packages and put them inside Assets/External Packages folder:
+BTM_Assets
+Generic Aircraft Models
+JMO Assets
+SlimUI
+TextMesh Pro
